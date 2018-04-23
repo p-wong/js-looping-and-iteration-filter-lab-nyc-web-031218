56e710c4f391153e1f43fafc47b947fb3f58fb26
p-wong <patriciawong08@gmail.com>
2018-04-23 13:34:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(function (name2) {
+    return name2.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch(data, letters) {
+  return data.filter(function (name) {
+    return name.startsWith(letters)
+  })
+}
+
+function matchName (data, name) {
+  return data.filter(function (array) {
+    return array.name === name
+  })
+}
